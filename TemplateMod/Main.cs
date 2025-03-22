3d7b88415c2062b84fa8f0e0354b1146b9e33d44
William Cunningham <williamcunningham.pro@gmail.com>
2025-03-22 18:07:39 +1100

    Initial commit

---
+++ TemplateMod/Main.cs
@@ -0,0 +1,19 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using Il2Cpp;
+using UnityEngine;
+using TemplateMod.Utils;
+
+[assembly: MelonInfo(typeof(TemplateMod.Main), "TemplateMod", "1.0.0", "MrCoolGuy640")]
+[assembly: MelonGame("LanPiaoPiao", "PlantsVsZombiesRH")]
+
+namespace TemplateMod
+{
+    public class Main : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            CoolguyLogger.Msg("Mod Started!");
+        }
+    }
+}
---
+++ TemplateMod/Utils/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq; // Add this to use FirstOrDefault()
+using MelonLoader;
+
+namespace TemplateMod.Utils
+{
+    public static class CoolguyLogger
+    {
+        private static readonly string ModName;
+
+        static CoolguyLogger()
+        {
+            // Get the mod info dynamically
+            var mod = MelonBase.RegisteredMelons.FirstOrDefault(m => m is Main);
+            if (mod != null)
+            {
+                ModName = $"[{mod.Info.Name} v{mod.Info.Version}]";
+            }
+            else
+            {
+                MelonLogger.Error("Failed to get mod name. Mods weren't initialized? - MrCoolGuy640");
+                ModName = "";
+            }
+        }
+
+        public static void Msg(string message)
+        {
+            MelonLogger.Msg($"{ModName} {message}");
+        }
+
+        public static void Warning(string message)
+        {
+            MelonLogger.Warning($"{ModName} {message}");
+        }
+
+        public static void Error(string message)
+        {
+            MelonLogger.Error($"{ModName} {message}");
+        }
+    }
+}
